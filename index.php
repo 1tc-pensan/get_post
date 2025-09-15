a3db9fda27bf7ba6f3dbb0318d5bd7fb01636b3b
1t-pensan <ppatrik0319@gmail.com>
2025-09-15 08:06:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+#űrlap(form) 1 mező ami nevet kér be és kiírja a képernyőre
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = htmlspecialchars($_POST['name']);
+//     echo "Hello, " . $name . "!";
+// } else {
+//     echo '<form method="post" action="">
+//             Name: <input type="text" name="name">
+//             <input type="submit" value="Submit">
+//           </form>';
+// }
+?>
+<html>
+<head>
+</head>
+<h1>Űrlap</h1>
+<form action="" method="post">
+    <label for="name"></label>
+    <input type="text" name="" id="">
+    <button tpye>Submit</button>
+</form>
+<body>
+</body>
+</html>
