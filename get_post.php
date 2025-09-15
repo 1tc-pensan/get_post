61a9b7f712f1c939488b9ac0dc8ceddfa3ac498e
1t-pensan <ppatrik0319@gmail.com>
2025-09-15 08:35:24 +0200

    form valami

--- get_post.php
+++ get_post.php
@@ -1,24 +1,39 @@
 <?php
-#űrlap(form) 1 mező ami nevet kér be és kiírja a képernyőre
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $name = htmlspecialchars($_POST['name']);
-//     echo "Hello, " . $name . "!";
-// } else {
-//     echo '<form method="post" action="">
-//             Name: <input type="text" name="name">
-//             <input type="submit" value="Submit">
-//           </form>';
-// }
+$name = '';
+$pass = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+    }
+    if (isset($_POST['pass'])) {
+        $pass = $_POST['pass'];
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['name'])) {
+    $name = $_GET['name'];
+}
 ?>
 <html>
 <head>
+    <title>Űrlap</title>
 </head>
-<h1>Űrlap</h1>
-<form action="" method="post">
-    <label for="name"></label>
-    <input type="text" name="" id="">
-    <button tpye>Submit</button>
-</form>
 <body>
+    <p><a href="?name=admin">Kattints ide a üdvözléshez</a></p>
+    <h1>Űrlap</h1>
+    <form action="" method="post">
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name">
+        <br>
+        <label for="pass">Jelszó:</label>
+        <input type="password" name="pass" id="pass">
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+    <?php if ($name): ?>
+        <p>Hello, <?php echo htmlspecialchars($name); ?>!</p>
+    <?php endif; ?>
+    <?php if ($pass): ?>
+        <p>Your password is: <?php echo htmlspecialchars($pass); ?></p>
+    <?php endif; ?>
 </body>
 </html>
