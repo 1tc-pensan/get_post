9fc09d676c87ea787bd8b9f7a8963e74c3bab751
1t-pensan <ppatrik0319@gmail.com>
2025-09-18 09:20:49 +0200

    sever.php kész

---
+++ server.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SERVER szuperglobális</title>
+</head>
+<body>
+    <h2></h2>
+    <ul>
+        <li><strong>Kérés módja:</strong><?php echo"$_SERVER[REQUEST_METHOD]"; ?></li>
+    </ul>
+</body>
+</html>
