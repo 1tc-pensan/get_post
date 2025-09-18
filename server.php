f4add0ee5e1a7a1b2f7a082153eb3835aad9f2ea
1t-pensan <ppatrik0319@gmail.com>
2025-09-18 09:49:43 +0200

    igen

--- server.php
+++ server.php
@@ -20,6 +20,21 @@
             echo " " . $_SERVER['QUERY_STRING'];
          }
          ?></li>
+    <h2>Szerver adati</h2>
+        <ul>
+            <li>Szerver neve : <?php echo $_SERVER['SERVER_NAME']; ?></li>
+            <li>Szerver IP címe : <?php echo $_SERVER['SERVER_ADDR']; ?></li>
+        </ul>
+        <h3>Felhasznnáló adatai</h3>
+        <ul>
+            <li>Felhasználó IP címe : <?php echo $_SERVER['REMOTE_ADDR']; ?></li>
+            <li>Felhasználó böngészője : <?php echo $_SERVER['HTTP_USER_AGENT']; ?></li>
+            <li>Felhasználó nyelve : <?php echo $_SERVER['HTTP_ACCEPT_LANGUAGE']; ?></li>
+            <li>Felhasználó host neve : <?php echo gethostbyaddr($_SERVER['REMOTE_ADDR']); ?></li>
+         <br>
+         <a href="?Patrik=mozso&age=12">
+         <p>Kattints ide egy paraméterezett GET kéréshez</p>
+         </a>
     </ul>
 </body>
 </html>
